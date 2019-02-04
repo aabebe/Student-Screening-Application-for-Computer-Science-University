b5af1bedbcd3bef5d67bf76ee3467b37667216b1
aabebe <aabebe@mum.edu>
2019-02-04 08:22:38 -0600

    routing for question is done

--- backend/db/DBConnection.js
+++ backend/db/DBConnection.js
@@ -1,22 +1,3 @@
-<<<<<<< HEAD
-const mongoose = require("mongoose");
-const dev_db_url = `mongodb://${process.env.DB_USER}:${process.env.DB_PASS}@${
-  process.env.DB_HOST
-}`;
-// const mongoDB = process.env.MONGODB_URI || dev_db_url;
-const mongoDB = dev_db_url;
-
-class DBConnection {
-  constructor() {
-    this.createConnection();
-  }
-  createConnection() {
-    mongoose.connect(mongoDB, () => {
-      console.log("DB is connected");
-    });
-    return mongoose;
-  } // end of connection
-=======
 const mongoose = require('mongoose');
 let ConstDB = require('../keys/ConstDB');
 const mongoUrl = `mongodb://${ConstDB.DB_USER}:${ConstDB.DB_PASS}@${ConstDB.DB_HOST}`;
@@ -37,7 +18,6 @@ class DBConnection {
     }// end of connection
 
 
->>>>>>> 5062706ccc37d482745997277d08755d8d8a6007
 }
 
 module.exports = DBConnection;
--- backend/routes/question.js
+++ backend/routes/question.js
@@ -2,9 +2,35 @@ const express = require("express");
 const router = express.Router();
 const Question = require("../models/question");
 
-router.get("/:id", (req, res, next) => {});
-router.get("/", (req, res) => {
-  res.status(200).json("get works");
+router.get("/:id", async (req, res, next) => {
+  const question = await Question.find({ questionId: req.params.id });
+  console.log(question);
+  if (!question)
+    res
+      .status(404)
+      .json({ error: "The question with the given id was not found" });
+
+  res.status(200).json(question);
+});
+router.put("/:id", async (req, res, next) => {
+  const question = await Question.update(
+    { questionId: req.params.id },
+    { status: "activated" },
+    { upsert: true, new: true }
+  );
+  if (!question)
+    return res
+      .status(404)
+      .json({ error: "The question with the given id was not found" });
+  res.status(200);
+});
+router.get("/", async (req, res, next) => {
+  try {
+    const question = await Question.find().sort("questionId");
+    res.status(200).json(question);
+  } catch (error) {
+    next(error);
+  }
 });
 router.post("/", async (req, res, next) => {
   let question = new Question({
@@ -12,7 +38,7 @@ router.post("/", async (req, res, next) => {
     isActivated: req.body.isActivated,
     questionId: req.body.questionId
   });
-  console.log("get the inputs");
+
   question = await question.save(question);
   res.status(200).json(question);
 });
