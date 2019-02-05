6c4eddb0bcbe5efa09a2eeec85445f656b34fe74
aabebe <aabebe@mum.edu>
2019-02-04 22:09:55 -0600

    ready to merge with mondays tasks

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -7,9 +7,13 @@ router.post("/", async (req, res) => {
   const password = req.body.password;
   Staff.findOne({ email: email, status: true }, function(err, staff) {
     console.log(staff);
-    if (!staff) return res.json({ status: 404, error: "Invalid Username" });
+    if (!staff)
+      return res.json({
+        status: 404,
+        error: "Invalid Username"
+      });
     if (staff.password !== password) {
-      res.json({ error: " Invalid Password" });
+      return res.json({ status: 404, error: " Invalid Password" });
     }
     // res.status(200).json(staff);
     const token = staff.generateAuthToken();
