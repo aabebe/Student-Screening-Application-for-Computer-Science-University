469f4e49a46f27d3b44f7f30d96678b658dc2b4a
aabebe <aabebe@mum.edu>
2019-02-04 19:21:54 -0600

    login page and authentication

--- backend/app.js
+++ backend/app.js
@@ -4,25 +4,27 @@ var path = require("path");
 var cookieParser = require("cookie-parser");
 var lessMiddleware = require("less-middleware");
 var logger = require("morgan");
-
+const cors = require("cors");
+const bodyParser = require("body-parser");
 const DataBase = require("./db/DBConnection");
 let db = new DataBase();
 
 var indexRouter = require("./routes/index");
 var usersRouter = require("./routes/users");
-var staffRouter = require('./routes/staff');
+var staffRouter = require("./routes/staff");
 const questionRouter = require("./routes/question");
-const authRouter = require('./routes/auth');
+const authRouter = require("./routes/auth");
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-var studentsRouter = require('./routes/student');
+var indexRouter = require("./routes/index");
+var usersRouter = require("./routes/users");
+var studentsRouter = require("./routes/student");
 
 var app = express();
 
 // view engine setup
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
+app.use(cors());
 
 app.use(logger("dev"));
 app.use(express.json());
@@ -31,18 +33,20 @@ app.use(cookieParser());
 app.use(lessMiddleware(path.join(__dirname, "public")));
 app.use(express.static(path.join(__dirname, "public")));
 
-app.use('/auth', authRouter);
+// app.use(bodyParser.json);
+// app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use("/auth", authRouter);
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
 app.use("/question", questionRouter);
-app.use(lessMiddleware(path.join(__dirname, 'public')));
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(lessMiddleware(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, "public")));
 
-
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-app.use('/students', studentsRouter);
-app.use('/staff', staffRouter);
+app.use("/", indexRouter);
+app.use("/users", usersRouter);
+app.use("/students", studentsRouter);
+app.use("/staff", staffRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
