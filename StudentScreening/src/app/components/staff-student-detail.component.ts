ef7a6f4d3e6d9dfaa06be7b8695afb201474f2d5
Bruck <bruckgmk@gmail.com>
2019-02-05 10:42:20 -0600

    Adjusted Student routing and and fetching on Components

--- StudentScreening/src/app/components/staff-student-detail.component.ts
+++ StudentScreening/src/app/components/staff-student-detail.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { StaffService } from '../services/staff.service';
 import { Subscription } from 'rxjs';
-
+import { StudentsService } from '../services/students.service';
 @Component({
   selector: 'app-staff-student-detail',
   template: `
@@ -15,8 +15,7 @@ import { Subscription } from 'rxjs';
             <tr>
               <th>First Name</th>
               <th>Last Name</th>
-              <th>Link</th>
-              <th>Check Status</th>
+              <th>Status</th>
             </tr>
           </thead>
           <tfoot>
@@ -37,15 +36,7 @@ import { Subscription } from 'rxjs';
                   Send Invitation
                 </button>
               </td>
-              <td>
-                <button
-                  class="uk-button uk-button-primary"
-                  [name]="stu._id"
-                  (click)="checkStatus(stu._id)"
-                >
-                  Check Status
-                </button>
-              </td>
+
             </tr>
           </tbody>
         </table>
@@ -58,33 +49,38 @@ export class StaffStudentDetailComponent implements OnInit {
   getStudentSubscriber: Subscription;
   students: any[] = [];
   isLoaded = false;
-  constructor(private staffService: StaffService) {
+  public studentList;
+  constructor(private staffService: StaffService, private studentsService: StudentsService) {
     console.log('I\'m in the constructorCon');
     const testing = staffService.getAllStaff();
-    console.log(testing);
+    console.log('Service Called');
+        studentsService.getStudentList().subscribe(data => {
+        this.studentList = data;
+    });
+    console.log(this.studentList);
   }
+
+
   sendInvitation(id) {
     console.log('test');
   }
-  checkStatus() {}
+  checkStatus() {
+
+  }
 
   ngOnInit() {
-    this.getStudentSubscriber = this.staffService
-      .getAllStaff()
+    this.getStudentSubscriber = this.studentsService
+      .getStudentList()
       .subscribe(data => {
         console.log('asffdfd: ', data[0]);
-        //  if(data['status'] ==200){
         this.isLoaded = true;
-        // tslint:disable-next-line:forin
         for (const i in data) {
           this.students.push(data[i]);
           console.log(data[i]);
         }
-        // this.students = data['data'];
-        // }
-        // else{
+
         console.log(this.students);
-        // }
+
       });
   }
 }
--- StudentScreening/src/app/keys/Keys.js
+++ StudentScreening/src/app/keys/Keys.js
@@ -2,7 +2,8 @@ const Keys = {
   API: {
     END_POINTS: {
       QUESTION: "http://localhost:4000/question",
-      ADMIN_STAFF: "http://localhost:4000/staff"
+      ADMIN_STAFF: "http://localhost:4000/staff",
+      ADMISSION_STAFF: "http://localhost:4000/students"
     },
     HEADER: {
       X_AUTH_TOKEN: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI1YzU4NmQ1NDc1ZWY5NTEzNmM5NDdjMGQiLCJyb2xlIjoiQWRtaW4iLCJlbWFpbCI6ImFkbWludXNlckBnbWFpbC5jb20iLCJpYXQiOjE1NDkzMDcwNDd9._5anBxUdha_CLh1DPh1JhBtnTwsN7CchkRXhjwwWAjs"
